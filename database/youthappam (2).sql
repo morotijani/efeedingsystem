c579b6f9dab84266815b0bda3895274c6d278586
morotijani <tjhackx111@gmail.com>
2024-09-17 19:46:53 +0000

    initial commit

---
+++ database/youthappam (2).sql
@@ -0,0 +1,406 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 17, 2024 at 09:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `youthappam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categories_id` int(11) NOT NULL,
+  `categories_name` varchar(255) NOT NULL,
+  `categories_active` int(11) NOT NULL DEFAULT 0,
+  `categories_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categories_id`, `categories_name`, `categories_active`, `categories_status`) VALUES
+(1, 'grains', 0, 1),
+(2, 'CEREALS', 0, 1),
+(3, 'oils', 0, 1),
+(4, 'Processed foods', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+
+CREATE TABLE `districts` (
+  `district_id` int(11) NOT NULL,
+  `district_name` varchar(100) DEFAULT NULL,
+  `district_admin` int(11) DEFAULT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `districts`
+--
+
+INSERT INTO `districts` (`district_id`, `district_name`, `district_admin`, `createdAt`) VALUES
+(2, 'Bantama', 10, '2024-09-09 01:15:33'),
+(3, 'navrongo', 6, '2024-09-09 01:27:00'),
+(8, 'Salaga', 11, '2024-09-09 06:03:22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manage_website`
+--
+
+CREATE TABLE `manage_website` (
+  `id` int(11) NOT NULL,
+  `title` varchar(600) NOT NULL,
+  `short_title` varchar(600) NOT NULL,
+  `logo` text NOT NULL,
+  `footer` text NOT NULL,
+  `currency_code` varchar(600) NOT NULL,
+  `currency_symbol` varchar(600) NOT NULL,
+  `login_logo` text NOT NULL,
+  `invoice_logo` text NOT NULL,
+  `background_login_image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `manage_website`
+--
+
+INSERT INTO `manage_website` (`id`, `title`, `short_title`, `logo`, `footer`, `currency_code`, `currency_symbol`, `login_logo`, `invoice_logo`, `background_login_image`) VALUES
+(1, 'Admin Panel by ', '9090908080', 'CK-LOGO-symbol-300x297.png', 'Admin PanelÂ', 'India', 'â‚¹', 'logo.png', 'logo.jpg', 'logo.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `client_name` varchar(255) NOT NULL,
+  `client_contact` varchar(255) NOT NULL,
+  `order_status` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(11) NOT NULL,
+  `file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_date`, `client_name`, `client_contact`, `order_status`, `user_id`, `file`) VALUES
+(1, '2024-09-09', '1', '0222222', 1, 3, ''),
+(2, '2024-09-09', '1', '0222222', 1, 3, ''),
+(3, '2024-09-09', '1', '0222222', 1, 3, ''),
+(4, '2024-09-09', '1', '0222222', 1, 3, ''),
+(5, '2024-09-09', '2', '0333333333', 1, 1, ''),
+(6, '2024-09-09', '1', '0222222', 1, 5, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL DEFAULT 0,
+  `product_id` int(11) NOT NULL DEFAULT 0,
+  `quantity` varchar(255) NOT NULL,
+  `order_item_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `order_item`
+--
+
+INSERT INTO `order_item` (`order_item_id`, `order_id`, `product_id`, `quantity`, `order_item_status`) VALUES
+(1, 4, 1, '5', 1),
+(2, 4, 2, '5', 1),
+(3, 5, 4, '500', 1),
+(4, 6, 1, '10', 1),
+(5, 6, 3, '50', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` text NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `rate` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `brand_id`, `categories_id`, `quantity`, `rate`, `active`, `status`) VALUES
+(1, 'beans', '', 0, 2, '285', '', 0, 1),
+(2, 'frytol', '', 0, 3, '95', '', 0, 1),
+(3, 'rice', '', 0, 1, '450', '', 0, 1),
+(4, 'Sardine', '', 0, 4, '500', '', 0, 1),
+(5, 'palm oil', '', 0, 3, '200', '', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_remarks`
+--
+
+CREATE TABLE `student_remarks` (
+  `remark_id` int(11) NOT NULL,
+  `student_id` varchar(100) NOT NULL,
+  `remarks` text NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_remarks`
+--
+
+INSERT INTO `student_remarks` (`remark_id`, `student_id`, `remarks`, `createdAt`) VALUES
+(1, '20210404111', 'bla bla', '2024-09-09 04:44:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_client`
+--
+
+CREATE TABLE `tbl_client` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `mob_no` varchar(150) NOT NULL,
+  `reffering` varchar(150) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `school_bd` enum('day','boarding') DEFAULT NULL,
+  `school_gender` enum('mix','single') DEFAULT NULL,
+  `school_population` int(11) NOT NULL,
+  `school_district` int(11) DEFAULT NULL,
+  `created_date_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `delete_status` int(11) NOT NULL,
+  `headmaster` int(11) DEFAULT NULL,
+  `storekeeper` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_client`
+--
+
+INSERT INTO `tbl_client` (`id`, `name`, `mob_no`, `reffering`, `address`, `school_bd`, `school_gender`, `school_population`, `school_district`, `created_date_time`, `delete_status`, `headmaster`, `storekeeper`) VALUES
+(1, 'Navrongo SHS', '0222222', '', 'Af-111-11', 'boarding', 'mix', 1000, 3, '2024-09-09 03:10:10', 0, 5, 3),
+(2, 'Presbytarian  Girls High School', '0333333333', '', '12', 'day', 'single', 2500, 2, '2024-09-09 03:05:45', 0, 8, 9),
+(4, 'Salaga shs', '0202223333', '', '111 tamale', 'boarding', 'mix', 100, 7, '2024-09-09 05:57:06', 0, 8, 3),
+(5, 'Salaga shs', '020183844', '', '1234 salaga', 'boarding', 'mix', 550, 8, '2024-09-09 06:07:37', 0, 5, 9),
+(6, 'Salaga SHTS', '0234576', '', '215', 'boarding', 'single', 555, 8, '2024-09-09 06:34:18', 0, 5, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_email_config`
+--
+
+CREATE TABLE `tbl_email_config` (
+  `e_id` int(21) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `mail_driver_host` varchar(5000) NOT NULL,
+  `mail_port` int(50) NOT NULL,
+  `mail_username` varchar(50) NOT NULL,
+  `mail_password` varchar(30) NOT NULL,
+  `mail_encrypt` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `permission` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `permission`) VALUES
+(1, 'Admin Abi', '5f4dcc3b5aa765d61d8327deb882cf99', 'admin@admin.com', 'admin,district,storekeeper,headmaster,national'),
+(3, 'calipo', '5f4dcc3b5aa765d61d8327deb882cf99', 'calscreations9@gmail.com', 'storekeeper'),
+(5, 'Nana Asamoah', '5f4dcc3b5aa765d61d8327deb882cf99', 'asamoah.headmaster@efeeding.com', 'headmaster,storekeeper'),
+(6, 'Baba Seidu', '5f4dcc3b5aa765d61d8327deb882cf99', 'baba.district@efeeding.com', 'district'),
+(7, 'Mr Aka', '5f4dcc3b5aa765d61d8327deb882cf99', 'aka.national@efeeding.com', 'national,district'),
+(8, 'raymond owusu', '5f4dcc3b5aa765d61d8327deb882cf99', 'raymond.headmaster@efeeding.com', 'headmaster,storekeeper'),
+(9, 'abigirl asare', '5f4dcc3b5aa765d61d8327deb882cf99', 'abigirl.storekeeper@efeeding.com', 'storekeeper'),
+(10, 'edward adjei', '5f4dcc3b5aa765d61d8327deb882cf99', 'edward.district@efeeding.com', 'district'),
+(11, 'Abudu Andani', '5f4dcc3b5aa765d61d8327deb882cf99', 'abudu.district@email.com', 'district');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categories_id`);
+
+--
+-- Indexes for table `districts`
+--
+ALTER TABLE `districts`
+  ADD PRIMARY KEY (`district_id`);
+
+--
+-- Indexes for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`order_item_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `student_remarks`
+--
+ALTER TABLE `student_remarks`
+  ADD PRIMARY KEY (`remark_id`);
+
+--
+-- Indexes for table `tbl_client`
+--
+ALTER TABLE `tbl_client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_email_config`
+--
+ALTER TABLE `tbl_email_config`
+  ADD PRIMARY KEY (`e_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `districts`
+--
+ALTER TABLE `districts`
+  MODIFY `district_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `manage_website`
+--
+ALTER TABLE `manage_website`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student_remarks`
+--
+ALTER TABLE `student_remarks`
+  MODIFY `remark_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_client`
+--
+ALTER TABLE `tbl_client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_email_config`
+--
+ALTER TABLE `tbl_email_config`
+  MODIFY `e_id` int(21) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
